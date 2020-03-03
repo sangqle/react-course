d2c36ec7b25d90bb5b173deef985b5945b47118c
littlemonster00 <sang.lequang94@gmail.com>
2020-03-03 10:06:22 +0700

    Add Expenses reducer testing

--- expensify-app/src/tests/selectors/expenses.test.js
+++ expensify-app/src/tests/selectors/expenses.test.js
@@ -1,33 +1,7 @@
 import selectExpenses from "../../selectors/expenses";
 import moment from "moment";
+import expenses from "../fixtures/expenses";
 
-const expenses = [
-  {
-    id: "1",
-    description: "Rent",
-    note: "",
-    amount: 234,
-    createdAt: 0
-  },
-  {
-    id: "2",
-    description: "Create card",
-    note: "",
-    amount: 100,
-    createdAt: moment(0)
-      .subtract(4, "day")
-      .valueOf()
-  },
-  {
-    id: "3",
-    description: "Gum",
-    note: "",
-    amount: 4000,
-    createdAt: moment(0)
-      .subtract(4, "day")
-      .valueOf()
-  }
-];
 test("should filtes by text value", () => {
   const filters = {
     text: "e",
