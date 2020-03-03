b00526ef23e7de8dbf4aa0fcbc656c4cf93c48fe
littlemonster00 <sang.lequang94@gmail.com>
2020-03-03 15:56:24 +0700

    Mocking libraries with jest:

---
+++ expensify-app/src/tests/components/ExpenseForm.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { shallow } from "enzyme";
+import ExpenseForm from "../../components/ExpenseForm";
+import expenses from "../fixtures/expenses";
+
+test("should render ExpenseForm correctly", () => {
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test("should render ExpenseForm with data", () => {
+  const wrapper = shallow(<ExpenseForm expenses={expenses[0]} />);
+  expect(wrapper).toMatchSnapshot();
+});
