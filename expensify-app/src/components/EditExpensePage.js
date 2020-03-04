7a82b25a15cb6b861a6a4aabba411e1a6aba64ec
littlemonster00 <sang.lequang94@gmail.com>
2020-03-04 15:30:04 +0700

    Add EditExpensePage testing

--- expensify-app/src/components/EditExpensePage.js
+++ expensify-app/src/components/EditExpensePage.js
@@ -2,27 +2,25 @@ import React from "react";
 import { connect } from "react-redux";
 import ExpenseForm from "./ExpenseForm";
 import { editExpense, removeExpense } from "../actions/expenses";
-const EditExpense = props => {
-  return (
-    <div>
-      <ExpenseForm
-        expense={props.expense}
-        onSubmit={expense => {
-          props.dispatch(editExpense(props.expense.id, expense));
-          props.history.push("/");
-        }}
-      />
-      <button
-        onClick={() => {
-          props.dispatch(removeExpense({ id: props.expense.id }));
-          props.history.push("/");
-        }}
-      >
-        Remove
-      </button>
-    </div>
-  );
-};
+
+export class EditExpensePage extends React.Component {
+  onSubmit = expense => {
+    this.props.editExpense(this.props.expense.id, expense);
+    this.props.history.push("/");
+  };
+  onRemove = () => {
+    this.props.removeExpense({ id: this.props.expense.id });
+    this.props.history.push("/");
+  };
+  render() {
+    return (
+      <div>
+        <ExpenseForm expense={this.props.expense} onSubmit={this.onSubmit} />
+        <button onClick={this.onRemove}>Remove</button>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state, props) => {
   return {
@@ -31,4 +29,10 @@ const mapStateToProps = (state, props) => {
     )
   };
 };
-export default connect(mapStateToProps)(EditExpense);
+const mapDispatchToProps = dispatch => {
+  return {
+    editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+    removeExpense: data => dispatch(removeExpense(data))
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
